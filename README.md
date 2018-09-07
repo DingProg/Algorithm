c2c4039e0c33319495661fcb948c98da3b322da5
dingdegao <dingdegao@guazi.com>
2018-09-07 14:52:18 +0800

    添加二叉树已知前，中序，求后序算法

--- README.md
+++ README.md
@@ -9,4 +9,5 @@
 [组合问题C(n,m):n个元素中取m个](https://github.com/DingProg/Algorithm/blob/master/%E9%80%92%E5%BD%92%E7%AE%97%E6%B3%95/combine.cpp)
 
 ## 二叉树相关
-[二叉树的基本操作](https://github.com/DingProg/Algorithm/blob/master/%E4%BA%8C%E5%8F%89%E6%A0%91/binary_tree.cpp)
+- [二叉树的基本操作](https://github.com/DingProg/Algorithm/blob/master/%E4%BA%8C%E5%8F%89%E6%A0%91/binary_tree.cpp)  
+- [已知二叉树的中序遍历和前序遍历，如何求后序遍历](https://github.com/DingProg/Algorithm/blob/master/%E4%BA%8C%E5%8F%89%E6%A0%91/binaryTree_fromPreInorder.cpp.cpp)
---
+++ 二叉树/binaryTree_fromPreInorder.cpp
@@ -0,0 +1,67 @@
+
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+typedef struct STreeNode{
+    int value;
+    STreeNode * left;
+    STreeNode * right;
+}TreeNode;
+
+
+/**
+ *         10
+ *     6          14
+ *  4   8      12   16
+ 
+ pre:10 6 4 8 14 12 16
+ inOrder:4 6 8 10 12 14 16
+ post:4 8 6 12 16 14 10
+ */
+
+TreeNode * initTreeFromOrder(int * pre,int *inOrder, int length){
+    if(length <= 0){
+        return NULL;
+    }
+    TreeNode *root = new TreeNode();
+    root->value = *pre;
+    root->left = NULL;
+    root->right = NULL;
+    
+    int index = 0;
+    
+    for(;index < length;index++){
+        if(*pre == inOrder[index]){
+            break;
+        }
+    }
+    
+    root->left = initTreeFromOrder(pre + 1, inOrder, index);
+    root->right = initTreeFromOrder(pre + index + 1, inOrder +index + 1, length - (index + 1));
+    
+    return root;
+}
+
+/**
+ *
+ *后序遍历
+ **/
+void traversalPost(TreeNode * root){
+    if(root != NULL){
+        traversalPost(root->left);
+        traversalPost(root->right);
+        cout<<root->value<<" ";
+    }
+}
+
+int main()
+{
+    int pre[] ={10,6,4,8,14,12,16};
+    int in[] ={4,6,8,10,12,14,16};
+    TreeNode *root = initTreeFromOrder(pre,in,7);
+    traversalPost(root);
+    return 0;
+}
